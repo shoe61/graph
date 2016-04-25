65d306c625a7caf5a5e4cd8addbe22914b58764b
Scott Schumacher <scottmachershoe@yahoo.com>
2016-04-25 17:21:10 -0500

    Added files via upload

---
+++ DIGRAPH.cpp
@@ -0,0 +1,80 @@
+
+/*---------------------------------------------------------------------------
+UNIT NAME : DiGraph.cpp
+PURPOSE   : Directed Graph / Adjacency Matrix Implementation
+WRITTEN FOR : Data Structures
+WRITTEN BY : Scott Schumacher
+DATE : 18 April 16
+---------------------------------------------------------------------------*/
+#include "digraph.H"
+#include <iostream>
+using namespace std;
+
+
+DiGraph::DiGraph (int n)
+// PreCondition : Graph has not been created and 0 <= n < MaxVertices
+// PostCondition : Graph Graph is created and has n vertices and no edges
+// Error Condition : Error if n < 0 or n >+ MaxVertices
+{
+	adjList = new SortedList[n];
+} //DiGraph
+
+DiGraph::DiGraph(DiGraph & other)
+// Requires : other has been created and initialized
+// Ensures : other is copied to this graph
+// Checks : none
+{
+	
+}
+
+
+
+
+DiGraph::~DiGraph ()
+// PreCondition : Graph is created
+// PostCondition : Graph is not created
+// ErrorCondition : none
+{
+} //~DiGraph
+
+
+void DiGraph::SwapDiGraphs ( DiGraph & other)
+// PreCondition : this graph and other are created and initialized
+// PostCondition : this = #other and other = #this
+// ErrorCondition : none
+{
+  	
+} // swap DiGraphs
+
+
+void DiGraph::AddEdge (int X, int Y)
+// PreCondition : Graph is created, X and Y are distinct vertices in Graph, and
+//                edge (X,Y) not in Graph
+// PostCondition : Graph is the same except an edge (X,Y) now exists
+// ErrorCondition : Error if X or Y is not
+//                  a vertex in Graph or if X and Y are not distinct or if
+//                  edge (X,Y) is already in Graph
+{
+	adjList[X].insertitem(Y);
+}  // AddEdge
+
+
+bool DiGraph::IsEdge (int X, int Y ) const
+// PreCondition : Graph is created, X and Y are vertices in Graph
+// PostCondition : Graph is unchanged, IsEdge is true if (f edge (X,Y) is in Graph
+// ErrorCondition : X or Y is not a vertex in Graph
+{
+	 
+	return false;
+} //IsEdge
+
+int DiGraph::GetNumberOfVertices ( ) const
+// PreCondition : Graph is created
+// PostCondition : Graph is unchanged, GetNumberofVertices is the number of
+//                 vertices in Graph
+// ErrorCondition : none
+{
+	return 0;
+}    // GetNumberOfVertices
+
+
